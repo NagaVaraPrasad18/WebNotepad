5a95a06150dd5088867637d4bb75709fd64cf921
NagaVaraPrasad <nagamallinagavaraprasad@gmail.com>
2025-07-27 21:08:20 +0530

    Changes - Monaco editor in searchresults, diff editor, icons

--- assets/javascript-BMAFSK2s.js
+++ assets/javascript-BMAFSK2s.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-ChLLRW3L.js";import"./index-D_BUMtHy.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-CjRjUVNL.js";import"./index-BQO9XlLt.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.45.0(5e5af013f8d295555a7210df0d5f2cea0bf5dd56)
  * Released under the MIT license

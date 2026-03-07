db5dbf2dc45f5d67f16158c79dcd09c3502d8e36
NagaVaraPrasad <nagamallinagavaraprasad@gmail.com>
2026-03-07 14:09:49 +0530

    Settings fix for qwen3 4b

--- assets/javascript-dMMvNlh9.js
+++ assets/javascript-dMMvNlh9.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-BsWJEnaG.js";import"./index-BARV5zyN.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-Cwh70qZs.js";import"./index-5hevk7_m.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.45.0(5e5af013f8d295555a7210df0d5f2cea0bf5dd56)
  * Released under the MIT license
--- assets/tsMode-zYY0aKs0.js
+++ assets/tsMode-zYY0aKs0.js
@@ -1,4 +1,4 @@
-import{t as O,m as I}from"./index-BARV5zyN.js";/*!-----------------------------------------------------------------------------
+import{t as O,m as I}from"./index-5hevk7_m.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.45.0(5e5af013f8d295555a7210df0d5f2cea0bf5dd56)
  * Released under the MIT license
